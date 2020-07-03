4d4209c860015568e87c07ae7c343a1734f95a06
sierras <783114047@qq.com>
2020-07-03 20:04:21 +0800

    update

--- render/src/plotify.py
+++ render/src/plotify.py
@@ -1,26 +1,69 @@
 import json
 from matplotlib import pyplot as plt
 
-event_prop = json.loads(open('proportion.json', 'r').read())
 
-parseHTML = []
-layout = []
-evaluateScript = []
+def event_prop():
+    event_prop = json.loads(open('proportion.json', 'r').read())
 
-for p in event_prop['res']:
-    parseHTML.append(p['ParseHTML'])
-    layout.append(p['Layout'])
-    evaluateScript.append(p['EvaluateScript'])
+    parseHTML = []
+    layout = []
+    evaluateScript = []
 
-parseHTML.sort()
-layout.sort()
-evaluateScript.sort()
+    for p in event_prop['res']:
+        parseHTML.append(p['ParseHTML'])
+        layout.append(p['Layout'])
+        evaluateScript.append(p['EvaluateScript'])
 
-plt.plot(parseHTML, marker='.')
-plt.plot(layout, marker='o')
-plt.plot(evaluateScript, marker='x')
+    parseHTML.sort()
+    layout.sort()
+    evaluateScript.sort()
 
-plt.legend(['ParseHTML', 'Layout', 'EvaluateScript'])
-plt.ylabel('Duration Proportion')
-plt.title('Duration Proportion of Different Events')
-plt.show()
+    plt.plot(parseHTML, marker='.')
+    plt.plot(layout, marker='o')
+    plt.plot(evaluateScript, marker='x')
+
+    plt.legend(['ParseHTML', 'Layout', 'EvaluateScript'])
+    plt.ylabel('Duration Proportion')
+    plt.title('Duration Proportion of Different Events')
+    plt.show()
+
+
+def running_duration():
+    event_prop = json.loads(open('running_duration.json', 'r').read())
+
+    renderer_main = []
+    compositor = []
+    tileworker = []
+
+    for p in event_prop['res']:
+        v = 0
+        for k in p:
+            if k == 'CrRendererMain':
+                p[k] = max(0.00001, p[k])
+                renderer_main.append(p[k])
+            elif k == 'Compositor':
+                p[k] = max(0.00001, p[k])
+                compositor.append(p[k])
+            elif k.startswith('CompositorTileWorker'):
+                v += p[k]
+
+        v = max(0.00001, v)
+        tileworker.append(v)
+
+    renderer_main.sort()
+    compositor.sort()
+    tileworker.sort()
+
+    plt.plot(renderer_main, marker='o')
+    plt.plot(compositor, marker='.')
+    plt.plot(tileworker, marker='x')
+    
+    plt.yscale('log')
+    plt.ylim([0.00001, 0.5])
+    plt.legend(['Renderer', 'Compositor', 'TileWorker'])
+    plt.ylabel('Running Duration')
+    plt.title('Running Duration Proportion of Different Threads')
+    plt.show()
+
+
+running_duration()
