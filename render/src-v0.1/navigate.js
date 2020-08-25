7986c3dd85e117f1b4f6e231df138681c1d096d7
Halflight <783114047@qq.com>
2020-08-25 13:48:37 +0800

    sync

--- render/src-v0.1/navigate.js
+++ render/src-v0.1/navigate.js
@@ -1,11 +1,13 @@
 const puppeteer = require('puppeteer');
+const EventEmitter = require('events');
 const delay = require('delay');
+const readline = require("readline");
 
-process.on('uncaughtException', function(error) {
+process.on('uncaughtException', function (error) {
     console.log(`Uncaught exceptionL ${error.message}`);
 });
 
-process.on('unhandledRejection', function(error) {
+process.on('unhandledRejection', function (error) {
     console.log(`Unhandled rejection: ${error.message}`);
 });
 
@@ -20,13 +22,13 @@ function formatDate() {
     return output;
 }
 
-async function navigate(url) {
+async function navigateCloseOnLoad(url) {
     var browser = await puppeteer.launch();
     var page = await browser.newPage();
     await page.setViewport({ width: 1600, height: 900 });
 
     var filename = `traces/${formatDate()}.json`;
-    page.on('load', async() => {
+    page.on('load', async () => {
         console.log('Page loaded.');
         await delay(3000);
 
@@ -44,4 +46,43 @@ async function navigate(url) {
     await page.goto(url);
 }
 
-navigate('http://localhost:8000/taobao.com');
+function navigateUserPrompt(url) {
+    var userInput = new EventEmitter();
+
+    async function navigate() {
+        var browser = await puppeteer.launch({ headless: false });
+        var page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
+
+        userInput.on('close', async () => {
+            console.log('Saving tracing data and closing browser...')
+
+            await delay(1000);
+            await page.tracing.stop();
+            await page.close();
+            await browser.close();
+
+            console.log('Browser closed...');
+        });
+
+        var filename = `traces/${formatDate()}.json`;
+        await page.tracing.start({ path: `${filename}` });
+        await page.goto(url);
+    }
+
+    navigate();
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Type "stop" to stop navigation: ', function (res) {
+        userInput.emit('close', {});
+        rl.close();
+    })
+}
+
+// navigateCloseOnLoad('http://localhost:8000/taobao.com');
+
+navigateUserPrompt('http://localhost:8000/image-layering/');
