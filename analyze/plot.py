2b734bb182172be039b45a29c53e08b319ec90f5
halflight <783114047@qq.com>
2020-11-20 19:22:26 +0800

    update

--- analyze/plot.py
+++ analyze/plot.py
@@ -7,11 +7,12 @@ from sklearn import linear_model
 import pandas
 import statsmodels.formula.api as formula
 
+
 def mean(array):
     return sum(array) / len(array)
 
 
-# R-Squared value for linear regression.
+# This equals to the R-Squared value of a linear regression.
 def explainable_variance(origin, prediction):
     m1 = mean(origin)
     m2 = mean(prediction)
@@ -20,33 +21,38 @@ def explainable_variance(origin, prediction):
     return var2 / var1
 
 
-# Predictor data form: [[x11, x12, ... , x1n], [x21, x22, ... , x2n], ...]
+# Predictor data form: [[list for variable X1], [list for variable X2], ...]
 # Criterion data form: [y1, y2, ... , yn]
-def OLS(y, x1, x2):
-    data_frame = pandas.DataFrame({'Y': y, 'X1': x1, 'X2': x2})
-    result = formula.ols(formula='Y ~ X1 + X2', data=data_frame).fit()
+def OLS(y, x1, x2, x3, x4, x5, x6):
+    data_frame = pandas.DataFrame({'Y': y, 'X1': x1, 'X2': x2, 'X3': x3, 'X4': x4, 'X5': x5, 'X6': x6})
+    result = formula.ols(formula='Y ~ X1 + X2 + X3 + X4 + X5 + X6', data=data_frame).fit()
     print(result.summary())
+    print()
 
 
 # Predictor data form: [[x11, x21, x31, ...], ... , [x1n, x2n, x3n, ...]]
 # Criterion data form: [y1, y2, ... , yn]
-def lr_remove_10percent_outliers(predictors, criterion):
+def linear_regression_remove_10_percent_outliers(predictors, criterion):
     lr = linear_model.LinearRegression()
     lr.fit(predictors, criterion)
-    output = [lr.predict([v])[0] for v in predictors]
+    output = lr.predict(predictors)
 
-    residual = [(abs(criterion[i] - output[i]), i)
-                for i in range(len(criterion))]
+    residual = [(abs(criterion[i] - output[i]), i) for i in range(len(output))]
     residual.sort(key=lambda v: v[0])
     # remove 10% data points with highest residual
     residual = residual[:int(len(residual) * 0.9)]
 
     predictors_ = [predictors[v[1]] for v in residual]
     criterion_ = [criterion[v[1]] for v in residual]
-    lr.fit(predictors_, criterion_)
-    output_ = [lr.predict([v])[0] for v in predictors_]
 
-    print('Explainable Variance:', explainable_variance(criterion_, output_))
+    x1 = [v[0] for v in predictors_]
+    x2 = [v[1] for v in predictors_]
+    x3 = [v[2] for v in predictors_]
+    x4 = [v[3] for v in predictors_]
+    x5 = [v[4] for v in predictors_]
+    x6 = [v[5] for v in predictors_]
+
+    OLS(criterion_, x1, x2, x3, x4, x5, x6)
 
 
 def plot_ilc_to_ltd():
@@ -105,41 +111,18 @@ def plot_ilc_to_ltd():
 
     pyplot.show()
 
-    # Non-linear regression.
-    predictors = [[ilc_log[i], ilc_nlogn[i]] for i in range(len(ilc))]
-    lr = linear_model.LinearRegression()
-    lr.fit(predictors, ltd)
-    ltd_pre = [lr.predict([v])[0] for v in predictors]
-
-    residual = [[abs(ltd_pre[i] - ltd[i]), i] for i in range(len(ilc))]
-    residual.sort(key=lambda v: v[0])
-    # remove 10% data points with highest residual
-    residual = residual[:int(len(residual) * 0.9)]
-
-    predictors_re = [predictors[v[1]] for v in residual]
-    ltd_re = [ltd[v[1]] for v in residual]
-    lr.fit(predictors_re, ltd_re)
-    ltd_re_pre = [lr.predict([v])[0] for v in predictors_re]
-
-    # Explainable Variance: 0.7582616998205116
-    print('Explainable Variance:', explainable_variance(ltd_re, ltd_re_pre))
-
-    # Coefficient: [ 0.42389409 21.72108819 -0.03950713]
-    print('Coefficient:', lr.coef_)
-
-    # Intercept: -8.918462665730544
-    print('Intercept:', lr.intercept_)
-
 
 # TODO
-# 1. max or sum of top-5?
-# 2. compute r-value for LR.
-# 3. understand values.
+# 1. max or sum of top-5? [DONE]
+# 2. compute r-value for LR. [DONE]
+# 3. understand values. [DONE]
 def separated_nonlinear_fitting():
     numerical_data = json.loads(open('data-trace.json', 'r').read())['data']
+
     ilc = []
     ult = []        # UpdateLayoutTree.
     layout = []
+
     for item in numerical_data:
         if len(item['tds']) > 0:
             ilc.append(item['ilc'])
@@ -148,41 +131,20 @@ def separated_nonlinear_fitting():
             for arr in item['tds']:
                 list_ult.append(arr[4] / 1000)
                 list_layout.append(arr[5] / 1000)
-            ult.append(max(list_ult))
-            layout.append(max(list_layout))
+            list_ult.sort(reverse=True)
+            list_layout.sort(reverse=True)
+            ult.append(sum(list_ult[:5]))
+            layout.append(sum(list_layout[:5]))
 
+    ilc_n2 = [v**2 for v in ilc]
     ilc_log = [math.log(v) for v in ilc]
+    ilc_log2 = [math.log(v)**2 for v in ilc]
     ilc_nlogn = [v * math.log(v) for v in ilc]
+    ilc_n2logn = [v**2 * math.log(v) for v in ilc]
 
-    pyplot.subplot(241)
-    pyplot.scatter(ilc, ult, s=1)
-    pyplot.title('n-ULT')
+    predictors = [[ilc[i], ilc_log[i], ilc_nlogn[i], ilc_n2[i], ilc_log2[i], ilc_n2logn[i]] for i in range(len(ilc))]
+    linear_regression_remove_10_percent_outliers(predictors, ult)
+    linear_regression_remove_10_percent_outliers(predictors, layout)
 
-    pyplot.subplot(242)
-    pyplot.scatter(ilc_log, ult, s=1)
-    pyplot.title('log(n)-ULT')
-
-    pyplot.subplot(243)
-    pyplot.scatter(ilc_nlogn, ult, s=1)
-    pyplot.title('nlog(n)-ULT')
-
-    pyplot.subplot(244)
-    pyplot.scatter(ilc, layout, s=1)
-    pyplot.title('n-Layout')
-
-    pyplot.subplot(245)
-    pyplot.scatter(ilc_log, layout, s=1)
-    pyplot.title('log(n)-Layout')
-
-    pyplot.subplot(246)
-    pyplot.scatter(ilc_nlogn, layout, s=1)
-    pyplot.title('nlog(n)-Layout')
-
-    pyplot.show()
-
-    # predictors = [[ilc[i], ilc_log[i], ilc_nlogn[i]] for i in range(len(ilc))]
-    # lr_remove_10percent_outliers(predictors, ult)
-    # lr_remove_10percent_outliers(predictors, layout)
-    test(ult, ilc, ilc_log)
 
 separated_nonlinear_fitting()
